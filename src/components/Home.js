5b40386b12469c21cfb39b0ccad6d6e8bf9049c9
rishabh <rishabhjairath@gmail.com>
2020-06-01 02:15:56 +0530

    home done

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,32 +2,36 @@ import React from "react";
 import profilePic from "../assets/img/img_rishabh.jpg";
 import Social from "./Social";
 import { Link, animateScroll as scroll } from "react-scroll";
+import "./Home.scss";
 
 const Home = ({ id }) => {
   return (
     <div className="home" id={id}>
-      <div className="home__intro-section">
-        <h2>HI! I'M RISHABH</h2>
-        <h3>A web developer who loves to build things</h3>
-        <div>
-          <button className="home__cta-primary bold">
-            <Link
-              activeClass="active"
-              to="section2"
-              spy={true}
-              smooth={true}
-              offset={-80}
-              duration={700}
-            >
-              KNOW MORE
-            </Link>
-          </button>
-          <button className="home__cta-primary bold">DOWNLOAD RESUME</button>
+      <div className="homeCard">
+        <div className="leftHomeCard">
+          <p className="nametag">HI! I'M RISHABH</p>
+          <p className="infotag">A web developer who loves to build things</p>
+          <div>
+            <button className="home__cta-primary bold">
+              <Link
+                activeClass="active"
+                to="section2"
+                spy={true}
+                smooth={true}
+                offset={-80}
+                duration={700}
+              >
+                KNOW MORE
+              </Link>
+            </button>
+            <button className="home__cta-primary bold">DOWNLOAD RESUME</button>
+          </div>
         </div>
-      </div>
-      <div className="home__profile-section">
-        <img src={profilePic} className="profilePic"></img>
-        <Social></Social>
+
+        <div className="rightHomeCard">
+          <img src={profilePic} className="profilePic"></img>
+          <Social></Social>
+        </div>
       </div>
     </div>
   );
