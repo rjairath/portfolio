aa06d86e9c2f8d0514a4a22dc5b1dc600028232b
rishabh <rishabhjairath@gmail.com>
2020-06-18 02:56:10 +0530

    check

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from "react";
-import logo from "./logo.svg";
+import React from "react";
 import Navbar from "./components/Navbar";
 import Section from "./components/Section";
 import Home from "./components/Home";
