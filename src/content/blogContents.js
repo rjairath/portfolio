7ae007f3c16dd29033b83ba9a347c51186a6e141
rishabh <rishabhjairath@gmail.com>
2020-06-19 20:44:31 +0530

    fix

--- src/content/blogContents.js
+++ src/content/blogContents.js
@@ -7,15 +7,14 @@ const blogs = [
     description:
       "Making a simple server side rendered app using Angular Universal and utilising TransferState class to send data from the server to the client",
     img: angular,
-    link:
-      "https://rishabh-codes.netlify.app/posts/server-side-rendering-angular",
+    link: "https://blog.rishabhjairath.in/posts/server-side-rendering-angular",
   },
   {
     title: "Building a static web server with Express",
     description:
       "Here we build a static web server using plain NodeJS first, and then the same using Express. Express is faster and a lot more convenient",
     img: server,
-    link: "https://rishabh-codes.netlify.app/posts/static-web-server-express",
+    link: "https://blog.rishabhjairath.in/posts/static-web-server-express",
   },
 ];
 
