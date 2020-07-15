1be5d8cc33a7787bf7c0815377d8c1b0205827ce
rishabh <rishabhjairath@gmail.com>
2020-07-16 01:57:47 +0530

    resume update

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ import { Link, animateScroll as scroll } from "react-scroll";
 import "./Home.scss";
 
 const url =
-  "https://drive.google.com/file/d/1Bx-WdZZ16wc50FSadML2R6wDRavkvU3U/view?usp=sharing";
+  "https://drive.google.com/file/d/1LdNEJN-GQWOpIHWGoa-SnBNXeu3XC41I/view?usp=sharing";
 
 const Home = ({ id }) => {
   const downloadResume = () => {
